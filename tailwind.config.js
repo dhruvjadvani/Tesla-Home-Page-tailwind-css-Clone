56568f405f906724544196afb560ee9deff5a49f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-11-15 15:48:40 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "tesla-dark": "#393c41",
+      }
+    },
+  },
+  plugins: [],
+}
 
